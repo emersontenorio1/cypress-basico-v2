00239209dedb7bcdfa5a4ae71037e707b3fac86f
Emerson Barbosa Tenorio <emersontenorio1@gmail.com>
2022-08-28 17:53:56 -0300

    Create cypress project

--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -122,25 +122,76 @@
         })
      });
 
-     it('seleciona todos os produto  por seu índice', () => {
-        // cy.get('select').invoke('text').then(($value) => {
-        //     cy.wrap($value)
+    //  it('seleciona todos os produto  por seu índice', () => {
+    //     // cy.get('select').invoke('text').then(($value) => {
+    //     //     cy.wrap($value)
            
             
-        //     //.should('have.value',$value )
-        // })
+    //     //     //.should('have.value',$value )
+    //     // })
 
-    })  
-    it.only('marca ambos checkboxes, depois desmarca o último', () => {
+    // })  
+    it('marca ambos checkboxes, depois desmarca o último', () => {
         cy.get('input[type=checkbox]')
         .check()
+        .should('be.checked')
         .uncheck(['phone'])
         .should('not.be.checked')
 
     });
 
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+        cy.get('#firstName').type('Emerson');
+        cy.get('#lastName').type('B Tenório');
+        cy.get('#email').type('exemplo@exemplo.com');
+        cy.get('#phone-checkbox').check();
+        cy.get('#open-text-area').type('test');
+        cy.get('[class="button"]').click();
+        cy.get('.error').should('be.visible');
+    });
 
+    //usar isso na bio se eu ainda estiver lá kkkkk
+    it('seleciona um arquivo da pasta fixture', () => {
+        cy.get('input[type="file"]')
+            .should('not.have.value')
+            .selectFile('cypress/fixtures/example.json')
+            .should(($input)=>{
+                expect($input[0].files[0].name).to.equal('example.json');
+            })
+    });
+    //isso sim eu vou usar muito mesmo na bio
+    it('seleciona um arquivo simulando um drag-and-drop', () => {
+        cy.get('input[type="file"]')
+        .should('not.have.value')
+        .selectFile('cypress/fixtures/example.json',{action:'drag-drop'})
+        .should(($input)=>{
+            expect($input[0].files[0].name).to.equal('example.json');
+        })
+    });
 
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+        cy.fixture('example.json').as('sampleFile')
+        cy.get('input[type="file"]')
+        .should('not.have.value')
+        .selectFile('@sampleFile')
+        .should(($input)=>{
+            expect($input[0].files[0].name).to.equal('example.json');
+        })
+    });
 
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+        cy.get('#privacy a').should('have.attr', 'target', '_blank')
+        .invoke('removeAttr', 'target')
+        .click();
+        
+    });
 
+    it('acessa a página da política de privacidade removendo o target e então clicanco no link', () => {
+        cy.get('#privacy a')
+        .invoke('removeAttr', 'target')
+        .click();
+        cy.contains('Talking About Testing').should('be.visible');
+        
+    });
+  
 })
---
+++ cypress/e2e/privacy.cy.js
@@ -0,0 +1,3 @@
+it('testa a página da política de privavidade de forma independente', () => {
+    cy.visit("../../src/privacy.html").contains('Talking About Testing').should('be.visible');
+});
